e757362088f864cff55b8bb8cbb6d0caf5260995
JosuePercy <josuehuallullo12@gmail.com>
2023-10-04 17:26:28 -0500

    feat: new project lifeCycle

---
+++ src/app/products/components/price/price.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, OnChanges, OnDestroy, SimpleChanges, Input } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
+
+@Component({
+  selector: 'products-price',
+  templateUrl: './price.component.html',
+  styleUrls: ['./price.component.css'],
+})
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+  @Input()
+  public price: number = 0;
+
+  public interval$?: Subscription;
+
+  ngOnInit(): void {
+    console.log('Componente HIJO: ngOnInit');
+
+    this.interval$ = interval(1000).subscribe((value) => console.log(`Tick: ${value}`));
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Componente HIJO: ngOnChanges');
+    console.log('price', { changes });
+  }
+  ngOnDestroy(): void {
+    console.log('Componente HIJO: ngOnDestroy');
+    this.interval$?.unsubscribe()
+  }
+}
